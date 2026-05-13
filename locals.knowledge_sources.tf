121004c51eae22f5eac9cd743ae5317107b45821
Olivia Durman <olivia.durman@deop.ca>
2026-05-13 17:28:14 -0400

    locals knowledge sources tf

--- locals.knowledge_sources.tf
+++ locals.knowledge_sources.tf
@@ -14,8 +14,8 @@ locals {
       marketplace_partner_resource_id          = null
     }
   } : {})
-  ks_ai_search_name             = try(var.ks_ai_search_definition.name, null) != null ? var.ks_ai_search_definition.name : (var.name_prefix != null ? "${var.name_prefix}-ks-ai-search" : "ai-alz-ks-ai-search-${random_string.name_suffix.result}")
+  ks_ai_search_name             = try(var.ks_ai_search_definition.name, null) != null ? var.ks_ai_search_definition.name : (var.name_prefix != null ? "${var.name_prefix}-ks-ai-search-${var.workload_code}-${lower(var.location)}-${var.instance_suffix}" : "ai-alz-ks-ai-search-${random_string.name_suffix.result}")
   ks_ai_search_role_assignments = try(var.ks_ai_search_definition.role_assignments, {})
-  ks_bing_grounding_name        = try(var.ks_bing_grounding_definition.name, null) != null ? var.ks_bing_grounding_definition.name : (var.name_prefix != null ? "${var.name_prefix}-ks-bing-grounding" : "ai-alz-ks-bing-grounding-${random_string.name_suffix.result}")
+  ks_bing_grounding_name        = try(var.ks_bing_grounding_definition.name, null) != null ? var.ks_bing_grounding_definition.name : (var.name_prefix != null ? "${var.name_prefix}-ks-bing-grounding-${var.workload_code}-${lower(var.location)}-${var.instance_suffix}" : "ai-alz-ks-bing-grounding-${random_string.name_suffix.result}")
 }
 
