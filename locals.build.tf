87d19907679b9674dc7f628bfc03045058fa833c
Olivia Durman <olivia.durman@deop.ca>
2026-05-13 17:27:28 -0400

    fixed locals build tf

--- locals.build.tf
+++ locals.build.tf
@@ -1,3 +1,3 @@
 locals {
-  build_vm_name = try(var.buildvm_definition.name, null) != null ? var.buildvm_definition.name : (var.name_prefix != null ? "${var.name_prefix}-build" : "ai-alz-buildvm")
+  build_vm_name = try(var.buildvm_definition.name, null) != null ? var.buildvm_definition.name : (var.name_prefix != null ? "${var.name_prefix}-build-${var.workload_code}-${lower(var.location)}-${var.instance_suffix}" : "ai-alz-buildvm")
 }
